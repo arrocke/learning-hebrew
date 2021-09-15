8434f523e1e3b8ae32554a883db67491889ebb7d
Adrian Rocke <me@adrianrocke.com>
2021-09-14 21:30:40 -0500

    build: seed import/export

---
+++ seed/export.sh
@@ -0,0 +1,2 @@
+mongoexport --db language-learning --collection resources > seed/resources.seed
+mongoexport --db language-learning --collection plans > seed/plans.seed
---
+++ seed/import.sh
@@ -0,0 +1,2 @@
+mongoimport --db language-learning --collection resources --file seed/resources.seed
+mongoimport --db language-learning --collection plans --file seed/plans.seed
